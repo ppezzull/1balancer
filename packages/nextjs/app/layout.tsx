35393e2de06ff5220a2d9535f4d3e30fc365a86d
ppezz <107001549+ppezzull@users.noreply.github.com>
2025-07-27 14:44:51 +0200

    Initial commit

---
+++ packages/nextjs/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
+import { ThemeProvider } from "~~/components/ThemeProvider";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "Scaffold-ETH 2 App",
+  description: "Built with 🏗 Scaffold-ETH 2",
+});
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html suppressHydrationWarning className={``}>
+      <body>
+        <ThemeProvider themes={["light"]} defaultTheme="light">
+          <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default ScaffoldEthApp;
