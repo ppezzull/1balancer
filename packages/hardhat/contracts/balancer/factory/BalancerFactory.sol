6c324a640d8b4b41131cfcbf54c26adf818969c1
Pietro Pezzullo <pezzullopietro04@gmail.com>
2025-07-30 17:59:32 +0200

    1inch native contracts added

--- packages/hardhat/contracts/balancer/factory/BalancerFactory.sol
+++ packages/hardhat/contracts/balancer/factory/BalancerFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Balancer} from "../modules/Balancer.sol";
+import {Balancer} from "../modules/BaseBalancer.sol";
 
 /**
  * @title BalancerFactory
